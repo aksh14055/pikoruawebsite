04852508dc5fb227fdf9d72ef5b1c0e442d6c18a
aksh14055 <AkshatChaudhari.cse23@adaniuni.ac.in>
2026-06-24 12:24:13 +0530

    feat: add Google Analytics 4 integration component

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Script from "next/script";
 import { usePathname } from "next/navigation";
 import { env } from "@/lib/env";
@@ -9,9 +9,14 @@ import { trackPageView } from "@/lib/analytics";
 export function GoogleAnalytics() {
   const measurementId = env.GA4_MEASUREMENT_ID;
   const pathname = usePathname();
+  const hasMounted = useRef(false);
 
   useEffect(() => {
     if (!measurementId) return;
+    if (!hasMounted.current) {
+      hasMounted.current = true;
+      return;
+    }
     trackPageView(window.location.href);
   }, [measurementId, pathname]);
 
@@ -29,7 +34,7 @@ export function GoogleAnalytics() {
           function gtag(){dataLayer.push(arguments);}
           window.gtag = gtag;
           gtag('js', new Date());
-          gtag('config', '${measurementId}', { send_page_view: false });
+          gtag('config', '${measurementId}');
         `}
       </Script>
     </>
